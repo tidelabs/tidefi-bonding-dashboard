3fe2364b1d2747a5952acbe798f9a0f4e9e5d828
Jeff <galbraith64@gmail.com>
2022-11-15 15:00:13 -0700

    feat: add initial Validator Info page

---
+++ src/pages/ValidatorInfo.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-page>
+    {{ JSON.stringify(validator, null, 2) }}
+  </q-page>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import { useEntitiesStore } from 'src/stores/entities'
+
+export default {
+  name: 'ValidatorInfo',
+
+  setup () {
+    const route = useRoute()
+    const entitiesStore = useEntitiesStore()
+
+    const validator = computed(() => {
+      const v = entitiesStore.getValidators.find((val) => val.address === route.params.address)
+      if (v) {
+        console.log('Selected validator:', v)
+        return
+      }
+      return null
+    })
+
+    // console.log('Look up validator:', route.params.address)
+
+    return {
+      validator
+    }
+  }
+}
+</script>
