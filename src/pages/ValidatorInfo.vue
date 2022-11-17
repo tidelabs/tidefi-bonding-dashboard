c31e7aa2b44010ebf989c16741de51edb6b7e79b
Jeff <galbraith64@gmail.com>
2022-11-17 13:39:38 -0700

    feat: ValidatorInfo updates

--- src/pages/ValidatorInfo.vue
+++ src/pages/ValidatorInfo.vue
@@ -1,26 +1,33 @@
 <template>
-  <div v-if="validator === null" class="error-message column justify-center items-center">
-    <div class="q-mt-lg">
-      Validator address not found!
-      <q-icon :name="infoIcon">
-        <q-tooltip>
-          {{ $route.params.address }}
-        </q-tooltip>
-      </q-icon>
+  <q-page padding>
+    <div v-if="validator === null" class="error-message column justify-center items-center">
+      <div class="q-mt-lg">
+        Validator address not found!
+        <q-icon :name="infoIcon">
+          <q-tooltip>
+            {{ $route.params.address }}
+          </q-tooltip>
+        </q-icon>
+      </div>
+      <div>
+        Did you change networks while on this page?
+      </div>
     </div>
-    <div>
-      Did you change networks while on this page?
+    <div v-else class="column">
+      <div class="row justify-start items-center">
+        <div class="border-light identity-svg-wrapper" v-html="validator.identicon" />
+        <div class="validator-name">{{ validator.name }}<q-tooltip>{{ validator.address }}</q-tooltip></div>
+      </div>
     </div>
-  </div>
-
+  </q-page>
 </template>
 
 <script>
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 import { useEntitiesStore } from 'src/stores/entities'
-
-const infoIcon = 'M24 44C29.5228 44 34.5228 41.7614 38.1421 38.1421C41.7614 34.5228 44 29.5228 44 24C44 18.4772 41.7614 13.4772 38.1421 9.85786C34.5228 6.23858 29.5228 4 24 4C18.4772 4 13.4772 6.23858 9.85786 9.85786C6.23858 13.4772 4 18.4772 4 24C4 29.5228 6.23858 34.5228 9.85786 38.1421C13.4772 41.7614 18.4772 44 24 44Z@@fill:currentColor;fill-opacity:0.6;;stroke:currentColor;stroke-width:4;stroke-linejoin:round&&M24 11C25.3807 11 26.5 12.1193 26.5 13.5C26.5 14.8807 25.3807 16 24 16C22.6193 16 21.5 14.8807 21.5 13.5C21.5 12.1193 22.6193 11 24 11Z@@fill:none;fill-rule:evenodd;clip-rule:evenodd;fill:white;&&M24.5 34V20H23.5H22.5@@fill:none;stroke:white;stroke-width:4;stroke-linecap:round;stroke-linejoin:round;&&M21 34H28@@fill:none;stroke:white;stroke-width:4;stroke-linecap:round;stroke-linejoin:round;|0 0 48 48'
+// import { useClientStore } from 'src/stores/client'
+import { infoIcon } from 'assets/icons'
 
 export default {
   name: 'ValidatorInfo',
@@ -28,6 +35,7 @@ export default {
   setup () {
     const route = useRoute()
     const entitiesStore = useEntitiesStore()
+    // const clientStore = useClientStore()
 
     const validator = computed(() => {
       const v = entitiesStore.getValidators.find((val) => val.address === route.params.address)
@@ -38,7 +46,16 @@ export default {
       return null
     })
 
-    // console.log('Look up validator:', route.params.address)
+    // const previousHistoryErasCount = computed(() => clientStore.getPreviousHistoryErasCount)
+
+    // const erasList = computed(() => {
+    //   const erasList = []
+    //   const api = clientStore.client.api
+
+    //   Array.from(Array(previousHistoryErasCount.value)).forEach(async (_, i) => {
+    //     const eraStackers = await
+    //   })
+    // })
 
     return {
       validator,
@@ -52,5 +69,8 @@ export default {
 .error-message {
   font-size: 1.2rem;
   color: red;
+}
+.validator-name {
+  font-size: 1.6rem;
 }
 </style>
