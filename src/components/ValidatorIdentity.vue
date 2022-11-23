933769af7003796b0cefddeecb7614dcdb3ba1ce
Jeff <jeff.galbraith@semantic-network.com>
2022-11-23 14:05:44 -0700

    feat: add ValidatorIdentity component (making things more modular)

---
+++ src/components/ValidatorIdentity.vue
@@ -0,0 +1,127 @@
+<template>
+  <table class="identity-table">
+    <tbody>
+      <tr v-if="parentName">
+        <td>Parent Name:</td><td>{{ parentName }}</td>
+      </tr>
+      <tr v-if="superName">
+        <td>Super Name:</td><td>{{ superName }}</td>
+      </tr>
+      <tr v-if="email">
+        <td>Email:</td><td>{{ email }}</td>
+      </tr>
+      <tr v-if="legal">
+        <td>Legal:</td><td>{{ legal }}</td>
+      </tr>
+      <tr v-if="riot">
+        <td>Riot:</td><td>{{ riot }}</td>
+      </tr>
+      <tr v-if="twitter">
+        <td>Twitter:</td><td>{{ twitter }}</td>
+      </tr>
+      <tr v-if="web">
+        <td>Web:</td><td>{{ web }}</td>
+      </tr>
+    </tbody>
+  </table>
+
+</template>
+
+<script>
+import { computed } from 'vue'
+
+export default {
+  name: 'ValidatorIdentity',
+
+  props: {
+    validator: {
+      type: Object,
+      required: true
+    }
+  },
+
+  setup (props) {
+    const parentName = computed(() => {
+      if (props.validator.parent && props.validator.parent.identity.info.display !== 'None') {
+        return props.validator.parent.identity.info.display.Raw
+      }
+      return ''
+    })
+
+    const superName = computed(() => {
+      if (props.validator.super && props.validator.super.length > 0) {
+        return props.validator.super[ 1 ].Raw
+      }
+      return ''
+    })
+
+    const email = computed(() => {
+      if (props.validator.parent && props.validator.parent.identity.info.email !== 'None') {
+        return props.validator.parent.identity.info.email.Raw
+      }
+      if (props.validator.identity && props.validator.identity.info.email !== 'None') {
+        return props.validator.identity.info.email.Raw
+      }
+      return ''
+    })
+
+    const legal = computed(() => {
+      if (props.validator.parent && props.validator.parent.identity.info.legal !== 'None') {
+        return props.validator.parent.identity.info.legal.Raw
+      }
+      if (props.validator.identity && props.validator.identity.info.legal !== 'None') {
+        return props.validator.identity.info.legal.Raw
+      }
+      return ''
+    })
+
+    const riot = computed(() => {
+      if (props.validator.parent && props.validator.parent.identity.info.riot !== 'None') {
+        return props.validator.parent.identity.info.riot.Raw
+      }
+      if (props.validator.identity && props.validator.identity.info.riot !== 'None') {
+        return props.validator.identity.info.riot.Raw
+      }
+      return ''
+    })
+
+    const twitter = computed(() => {
+      if (props.validator.parent && props.validator.parent.identity.info.twitter !== 'None') {
+        return props.validator.parent.identity.info.twitter.Raw
+      }
+      if (props.validator.identity && props.validator.identity.info.twitter !== 'None') {
+        return props.validator.identity.info.twitter.Raw
+      }
+      return ''
+    })
+
+    const web = computed(() => {
+      if (props.validator.parent && props.validator.parent.identity.info.web !== 'None') {
+        return props.validator.parent.identity.info.web.Raw
+      }
+      if (props.validator.identity && props.validator.identity.info.web !== 'None') {
+        return props.validator.identity.info.web.Raw
+      }
+      return ''
+    })
+
+    return {
+      parentName,
+      superName,
+      email,
+      legal,
+      riot,
+      twitter,
+      web
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.identity-table {
+  border: 1px solid rgba(200,200,200,0.4);
+  border-radius: 4px;
+  margin-top: 8px;
+}
+</style>
--- src/pages/ValidatorInfo.vue
+++ src/pages/ValidatorInfo.vue
@@ -25,6 +25,7 @@
           option-label="name"
           map-options
           outlined
+          color="purple-13"
           label="Selected validator"
           style="min-width: 300px;"
         />
@@ -39,31 +40,7 @@
                 <div class="validator-name">{{ validator.name }}</div>
               </div>
               <div>{{ validator.address }}</div>
-              <table class="identity-table">
-                <tbody>
-                  <tr v-if="parentName">
-                    <td>Parent Name:</td><td>{{ parentName }}</td>
-                  </tr>
-                  <tr v-if="superName">
-                    <td>Super Name:</td><td>{{ superName }}</td>
-                  </tr>
-                  <tr v-if="email">
-                    <td>Email:</td><td>{{ email }}</td>
-                  </tr>
-                  <tr v-if="legal">
-                    <td>Legal:</td><td>{{ legal }}</td>
-                  </tr>
-                  <tr v-if="riot">
-                    <td>Riot:</td><td>{{ riot }}</td>
-                  </tr>
-                  <tr v-if="twitter">
-                    <td>Twitter:</td><td>{{ twitter }}</td>
-                  </tr>
-                  <tr v-if="web">
-                    <td>Web:</td><td>{{ web }}</td>
-                  </tr>
-                </tbody>
-              </table>
+              <ValidatorIdentity :validator="validator" />
             </div>
           </div>
         </div>
@@ -86,11 +63,13 @@ import { useClientStore } from 'src/stores/client'
 import { infoIcon } from 'assets/icons'
 
 import ValidatorRewardPoints from 'components/ValidatorRewardPoints.vue'
+import ValidatorIdentity from 'src/components/ValidatorIdentity.vue'
 
 export default {
   name: 'ValidatorInfo',
 
   components: {
+    ValidatorIdentity,
     ValidatorRewardPoints
   },
 
@@ -117,70 +96,6 @@ export default {
         return v
       }
       return null
-    })
-
-    const parentName = computed(() => {
-      if (validator.value.parent && validator.value.parent.identity.info.display !== 'None') {
-        return validator.value.parent.identity.info.display.Raw
-      }
-      return ''
-    })
-
-    const superName = computed(() => {
-      if (validator.value.super && validator.value.super.length > 0) {
-        return validator.value.super[ 1 ].Raw
-      }
-      return ''
-    })
-
-    const email = computed(() => {
-      if (validator.value.parent && validator.value.parent.identity.info.email !== 'None') {
-        return validator.value.parent.identity.info.email.Raw
-      }
-      if (validator.value.identity && validator.value.identity.info.email !== 'None') {
-        return validator.value.identity.info.email.Raw
-      }
-      return ''
-    })
-
-    const legal = computed(() => {
-      if (validator.value.parent && validator.value.parent.identity.info.legal !== 'None') {
-        return validator.value.parent.identity.info.legal.Raw
-      }
-      if (validator.value.identity && validator.value.identity.info.legal !== 'None') {
-        return validator.value.identity.info.legal.Raw
-      }
-      return ''
-    })
-
-    const riot = computed(() => {
-      if (validator.value.parent && validator.value.parent.identity.info.riot !== 'None') {
-        return validator.value.parent.identity.info.riot.Raw
-      }
-      if (validator.value.identity && validator.value.identity.info.riot !== 'None') {
-        return validator.value.identity.info.riot.Raw
-      }
-      return ''
-    })
-
-    const twitter = computed(() => {
-      if (validator.value.parent && validator.value.parent.identity.info.twitter !== 'None') {
-        return validator.value.parent.identity.info.twitter.Raw
-      }
-      if (validator.value.identity && validator.value.identity.info.twitter !== 'None') {
-        return validator.value.identity.info.twitter.Raw
-      }
-      return ''
-    })
-
-    const web = computed(() => {
-      if (validator.value.parent && validator.value.parent.identity.info.web !== 'None') {
-        return validator.value.parent.identity.info.web.Raw
-      }
-      if (validator.value.identity && validator.value.identity.info.web !== 'None') {
-        return validator.value.identity.info.web.Raw
-      }
-      return ''
     })
 
     watch(selectedValidator, (val) => {
@@ -217,14 +132,7 @@ export default {
       selectedValidator,
       validators,
       validator,
-      infoIcon,
-      parentName,
-      superName,
-      email,
-      legal,
-      riot,
-      twitter,
-      web
+      infoIcon
     }
   }
 }
@@ -237,9 +145,5 @@ export default {
 }
 .validator-name {
   font-size: 1.6rem;
-}
-.identity-table {
-  border: 1px solid rgba(200,200,200,0.4);
-  border-radius: 4px;
 }
 </style>
