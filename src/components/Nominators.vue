c68e1828723f7b9b75d2161eab56231bd31cb1d3
Jeff <jeff.galbraith@semantic-network.com>
2022-11-25 15:13:59 -0700

    feat: add Nominators panel

---
+++ src/components/Nominators.vue
@@ -0,0 +1,73 @@
+<template>
+  <q-card v-if="validator.nominatorCount > 0" class="info-table">
+    <q-scroll-area style="height: 200px;">
+      <table>
+        <thead>
+          <tr>
+            <th colspan="2">Nominators</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="{address, hashAddress, formattedValue, identicon} in nominators" :key="address">
+            <td>
+              <div class="row justify-start items-center">
+                <div class="border-light identity-svg-wrapper" v-html="identicon" />
+                <div>{{ hashAddress }}<q-tooltip>{{ address }}</q-tooltip></div>
+              </div>
+            </td>
+            <td class="text-right">
+              {{ formattedValue }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </q-scroll-area>
+  </q-card>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { toSvg } from 'jdenticon'
+import { trimHash, toBaseToken } from 'src/helpers/utils'
+import { useClientStore } from 'src/stores/client'
+
+export default {
+  name: 'Nominators',
+
+  props: {
+    validator: {
+      type: Object,
+      required: true
+    }
+  },
+
+  setup (props) {
+    const nominators = computed(() => {
+      const nominators = props.validator?.stakers?.others.map(({ who, value }) => {
+        return {
+          address: who,
+          hashAddress: trimHash(who, 16),
+          value,
+          formattedValue: formatTokenValue(value),
+          identicon: toSvg(who, 24)
+        }
+      })
+
+      // highest first
+      nominators.sort((a, b) => parseFloat(b.value) - parseFloat(a.value))
+
+      return nominators
+    }) ?? []
+
+    function formatTokenValue (val) {
+      const clientStore = useClientStore()
+
+      return clientStore.decimals.length > 0 ? toBaseToken(val, clientStore.decimals[ 0 ]) : 0
+    }
+
+    return {
+      nominators
+    }
+  }
+}
+</script>
