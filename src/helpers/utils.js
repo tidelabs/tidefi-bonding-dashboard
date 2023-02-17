8a3cf99badcbe06910bc977a9d6d994e9a469d39
Jeff <jeff.galbraith@semantic-network.com>
2023-02-17 09:08:14 -0700

    feat: Stakes panel enhancements

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -4,7 +4,7 @@ import { hexToU8a, isHex } from '@polkadot/util'
 import { useChainsStore } from 'stores/chain'
 import { useClientStore } from 'src/stores/client'
 import { Client } from '../classes/client'
-import { format, subDays } from 'date-fns'
+import { format, subDays, parseISO } from 'date-fns'
 
 export async function getAccountBalances (api, addr) {
   return await api.query.balances.account(addr)
@@ -110,4 +110,160 @@ export function eraToDate (era) {
     return format(subDays(new Date(), currentEra - era), 'MMM.dd')
   }
   return 'unknown'
+}
+
+export function blocksToMS (block) {
+  const clientStore = useClientStore()
+  const blockTime = parseInt(clientStore.consts.expectedBlockTime, 10) // 6000
+  return (parseInt(block, 10) * blockTime)
+}
+
+export function formatDateInternational (when) {
+  let date = when
+
+  if (!date) return ''
+  if (typeof date === 'string') date = parseISO(date)
+
+  return format(date, 'dd.LLL.yyyy')
+}
+
+/**
+ * Given a number of seconds, return a string that expresses the time in days, hours, minutes, and
+ * seconds.
+ * @param s - number in seconds,
+ * @param maxResolution - smallest time unit that return will show
+ * @param simple - Format to return | true: 1:02:03:22 | false: 1 day 2 hours 3 minutes 22 seconds
+ * @returns A string with the number of days, hours, minutes, and seconds.
+ */
+export function formatDurationFromSeconds (
+  s, // number,
+  maxResolution = 'seconds', // 'days' | 'hours' | 'minutes' | 'seconds'
+  simple = false
+) {
+  let remaining = s
+  const days = Math.floor(remaining / 86_400)
+  remaining = remaining % 86_400
+  const hours = Math.floor(remaining / 3_600)
+  remaining = remaining % 3_600
+  const minutes = Math.floor(remaining / 60)
+  remaining = remaining % 60
+  const seconds = remaining
+
+  const dayString = simple
+    ? `${ days.toString().padStart(2, '0') }:`
+    : days > 1
+      ? `${ days } Days`
+      : days === 1
+        ? `${ days } Day`
+        : maxResolution === 'days'
+          ? '0 Days'
+          : ''
+  const hourString = simple
+    ? `${ hours.toString().padStart(2, '0') }:`
+    : hours > 1
+      ? `${ hours } Hours`
+      : hours === 1
+        ? `${ hours } Hour`
+        : maxResolution === 'hours' && days === 0
+          ? '0 Hours'
+          : ''
+  const minuteString = simple
+    ? `${ minutes.toString().padStart(2, '0') }:`
+    : minutes > 1
+      ? `${ minutes } Minutes`
+      : minutes === 1
+        ? `${ minutes } Minute`
+        : maxResolution === 'minutes' && days === 0 && hours === 0
+          ? '0 Minutes'
+          : ''
+  const secondString = simple
+    ? seconds.toString().padStart(2, '0')
+    : seconds > 1
+      ? `${ seconds } Seconds`
+      : seconds === 1
+        ? `${ seconds } Second`
+        : maxResolution === 'seconds' && days === 0 && hours === 0 && minutes === 0
+          ? '0 Seconds'
+          : ''
+
+  let durationString = ''
+
+  switch (maxResolution) {
+    case 'seconds':
+      durationString
+        = `${ dayString } ${ hourString } ${ minuteString } ${ secondString }`.trim()
+      break
+    case 'minutes':
+      durationString = `${ dayString } ${ hourString } ${ minuteString }`.trim()
+      break
+    case 'hours':
+      durationString = `${ dayString } ${ hourString }`.trim()
+      break
+    case 'days':
+      durationString = `${ dayString }`.trim()
+      break
+  }
+
+  return durationString
+}
+
+/**
+ * Converts seconds to a time unit as a float.
+ * @param  s - number in seconds
+ * @param resolution - Unit to return.
+ * Can be days, hours, minutes, or seconds.
+ * @param decimals - The number of decimal places to show.
+ * @param cascade - Increase resolution unit when value falls below 1 of the current resolution.
+ * @param maxCascadeResolution - Smallest resolution cascade can go to.
+ * @example toTimeFloat(153_000) => [1.7, 'days']
+ * @returns Array(2) of value and unit.
+ */
+export function toTimeFloat (
+  s, // number
+  resolution = 'days', // 'days' | 'hours' | 'minutes' | 'seconds'
+  decimals = 1,
+  cascade = true,
+  maxCascadeResolution = 'seconds' // 'days' | 'hours' | 'minutes' | 'seconds'
+) {
+  if (s <= 0 || isNaN(s)) return [ 0, 'seconds' ]
+
+  const resolutions = [ 'days', 'hours', 'minutes', 'seconds' ]
+  let cascadePoint = resolutions.indexOf(resolution)
+  let value = 0
+
+  for (let i = resolutions.indexOf(resolution); i < resolutions.length; i++) {
+    switch (resolutions[ `${ i }` ]) {
+      case 'days':
+        value = s / 86_400
+        break
+      case 'hours':
+        value = s / 3_600
+        break
+      case 'minutes':
+        value = s / 60
+        break
+      case 'seconds':
+        value = s
+        break
+    }
+    if (value < 1 && cascade && i < resolutions.indexOf(maxCascadeResolution)) {
+      cascadePoint++
+    }
+    else {
+      break
+    }
+  }
+
+  let timeValue = value * Math.pow(10, decimals) // put decimal at max decimals spot
+  timeValue = Math.ceil(timeValue) // get rid of undesired decimals
+  timeValue = timeValue / Math.pow(10, decimals) // put decimal back in place
+
+  const unitString
+    = timeValue === 1
+      ? resolutions[ `${ cascadePoint }` ].slice(0, -1) // remove 's' when 1
+      : resolutions[ `${ cascadePoint }` ]
+
+  timeValue = timeValue || 0
+
+  return [ timeValue, unitString ]
 }
